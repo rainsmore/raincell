da420cd9af85e01391f63cb14995e3d8fdeb9ca1
Iñigo Reizabal Arregui <inigo.reizabal@ird.fr>
2025-11-24 12:19:49 +0100

    Set xarray config to avoid expanding coordinates on ds and da and data in da

--- raincell/config.py
+++ raincell/config.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+import xarray as xr
+
 
 LIBRARY_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = LIBRARY_DIR.parent
 SAMPLE_DATA_DIR = LIBRARY_DIR / "sample_data"
+
+xr.set_options(display_expand_coords=False, display_expand_data=False)
